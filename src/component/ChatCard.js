a9e838d478ac90a69e49e6f3dc23f231cb1816eb
karandas777 <karandas.21.08.98@gmail.com>
2020-07-20 10:40:16 +0530

    fixed padding

--- src/component/ChatCard.js
+++ src/component/ChatCard.js
@@ -5,13 +5,13 @@ export default class ChatCard extends Component {
         const {chat} = this.props;
         if(chat.username === localStorage.getItem('username')){
             return (
-                <div className="row my-3 mx-0" >
+                <div className="row my-4 mx-0" >
                     <div className="col-11 pl-0">
-                        <div className="bg-grad text-light p-3 rounded">
+                        <div className="bg-grad text-light px-3 pt-3 pb-1 rounded">
                             <div className="hand-written mb-2">{chat.username}</div>
                             <div className="h4 hand-written">{chat.message}</div>
+                            <div className="small text-right text-light px-2 mt-1">{chat.date}</div>
                         </div>
-                        <div className="small text-right text-light px-2 mt-1">{chat.date}</div>
                     </div>
                     <div className="col-1 p-0">
                         <img src={`https://avatars.dicebear.com/api/gridy/${chat.username}.svg?`} 
@@ -22,17 +22,17 @@ export default class ChatCard extends Component {
         }
         else{
             return (
-                <div className="row my-3 mx-0" >
+                <div className="row my-4 mx-0" >
                     <div className="col-1 p-0">
                         <img src={`https://avatars.dicebear.com/api/gridy/${chat.username}.svg?`} 
                             className="userimg w-100 h-auto bg-light rounded-pill p-1 mt-1" alt="user-img"></img>
                     </div>
                     <div className="col-11 pr-0">
-                        <div className="bg-grad text-light p-3 rounded">
+                        <div className="bg-grad text-light px-3 pt-3 pb-1 rounded">
                             <div className="hand-written mb-2">{chat.username}</div>
                             <div className="h4 hand-written">{chat.message}</div>
+                            <div className="small text-right text-light px-2 mt-1">{chat.date}</div>
                         </div>
-                        <div className="small text-light px-2 mt-1">{chat.date}</div>
                     </div>
                 </div>
             )
