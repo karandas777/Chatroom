3bf1997addfb5ff1cb53121304aa76f4103a93f4
karandas777 <karandas.21.08.98@gmail.com>
2020-07-20 10:34:13 +0530

    minor changes

--- src/component/ChatCard.js
+++ src/component/ChatCard.js
@@ -6,10 +6,10 @@ export default class ChatCard extends Component {
         if(chat.username === localStorage.getItem('username')){
             return (
                 <div className="row my-3 mx-0" >
-                    <div className="col-8 ml-auto pl-0">
+                    <div className="col-11 pl-0">
                         <div className="bg-grad text-light p-3 rounded">
                             <div className="hand-written mb-2">{chat.username}</div>
-                            <div className="h4 mb-0 hand-written">{chat.message}</div>
+                            <div className="h4 hand-written">{chat.message}</div>
                         </div>
                         <div className="small text-right text-light px-2 mt-1">{chat.date}</div>
                     </div>
@@ -27,10 +27,10 @@ export default class ChatCard extends Component {
                         <img src={`https://avatars.dicebear.com/api/gridy/${chat.username}.svg?`} 
                             className="userimg w-100 h-auto bg-light rounded-pill p-1 mt-1" alt="user-img"></img>
                     </div>
-                    <div className="col-8 pr-0">
+                    <div className="col-11 pr-0">
                         <div className="bg-grad text-light p-3 rounded">
                             <div className="hand-written mb-2">{chat.username}</div>
-                            <div className="h4 mb-0 hand-written">{chat.message}</div>
+                            <div className="h4 hand-written">{chat.message}</div>
                         </div>
                         <div className="small text-light px-2 mt-1">{chat.date}</div>
                     </div>
